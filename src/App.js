1734299c22b28aeae5711e1e406bf539ba1f6a75
Dessi Costa <desiree.costa@student.kyh.se>
2019-01-28 14:35:10 +0100

    googlemaps

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 import CurrentLocation from './Map';
 
+const markers = [
+  { position: {
+      lat: 59.308761,
+      lng: 18.282199},
+    title: 'Dolores park'},
+    { position: {lat: 59.308761,
+      lng: 18.283399},
+      title: 'Crillzors park'}
+  ];
+
 export class MapContainer extends Component {
   state = {
     showingInfoWindow: false,
@@ -26,10 +36,16 @@ export class MapContainer extends Component {
     }
   };
 
+
+
   render() {
     return (
       <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
         <Marker onClick={this.onMarkerClick} name={'current location'} />
+        {markers.map((marker, index) => <Marker key={index} {...marker} />
+        
+        )}
+        {/*<Marker name={'Dolores park'} position={{lat: 59.308761, lng: 18.282199}} />*/}
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
