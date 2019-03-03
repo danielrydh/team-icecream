ef1c1be4e55153d1f3465062feda03824a566655
Sara Överström <saraoverstrom87@gmail.com>
2019-03-03 20:35:45 +0100

    Have mad GameMap

--- src/constants/ui.js
+++ src/constants/ui.js
@@ -1,4 +1,8 @@
 import speaking_bubble from './assets/ui/speaking_bubble.png';
+import history from './assets/icons/history.png';
+import leaderboard from './assets/icons/leaderboard.png';
+import settings from './assets/icons/settings.png';
+import stats from './assets/icons/stats.png';
 
 import arrow16px from './assets/ui/arrow16px2.png';
 import arrow32px from './assets/ui/arrow32px2.png';
@@ -12,7 +16,11 @@ const ui = {
     arrow_large: arrow32px,
     arrow_thin: arrowThin,
     arrow_thin_reverse: arrowthinReverse,
-    speaking_thing: speaking_thing
+    speaking_thing: speaking_thing,
+    history: history,
+    leaderboard: leaderboard,
+    settings: settings,
+    stats: stats
   },
   background: {
     speaking_bubble: speaking_bubble
